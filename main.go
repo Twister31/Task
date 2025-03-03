21638c1b6aa575f0270b8dfc707f76ea45008a23
vlados <vlad.biliczkij@mail.ru>
2025-03-03 21:47:27 +0300

    Step 3 is done

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -19,6 +20,22 @@ func getTask(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(task)
 }
 
+func putTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	json.NewDecoder(r.Body).Decode(&task)
+	DB.Model(&task).Updates(task)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	json.NewDecoder(r.Body).Decode(&task)
+	id := task.ID
+	DB.Delete(&task)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Task with ID %d deleted", id)
+}
+
 func main() {
 
 	InitDB()
@@ -27,8 +44,10 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/hello", getTask).Methods("GET")
+	router.HandleFunc("/api/task", getTask).Methods("GET")
 	router.HandleFunc("/api/task", postTask).Methods("POST")
+	router.HandleFunc("/api/task", putTask).Methods("PUT")
+	router.HandleFunc("/api/task", deleteTask).Methods("DELETE")
 
 	http.ListenAndServe("localhost:8082", router)
 
